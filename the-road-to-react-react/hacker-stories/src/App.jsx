8e5e01a768b265f868583a28c49538750cc6ebac
IvanZhovnych <izhovnych@optimiz.ca>
2024-06-18 17:00:42 -0500

    Added map method to the list

--- the-road-to-react-react/hacker-stories/src/App.jsx
+++ the-road-to-react-react/hacker-stories/src/App.jsx
@@ -7,6 +7,25 @@ const welcome = {
   title: "Rocky",
 };
 
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 let fruits = ["apple", "banana", "orange", "grape"];
 
 function App() {
@@ -17,6 +36,24 @@ function App() {
         {fruits.map((fruit, index) => (
           <li key={index}>{fruit}</li>
         ))}
+      </ul>
+
+      {/*List*/}
+      <ul>
+        {list.map(function (item) {
+          return (
+            <div key={item.objectID}>
+            <li>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </li>
+            </div>
+          );
+        })}
       </ul>
     </div>
   );
